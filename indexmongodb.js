38175181a0e35b8db79b5503d5c98e07a0aa666d
crysosancher <vibhupandey.vp@gmail.com>
2022-12-27 21:45:55 +0530

    mongo db

--- indexmongodb.js
+++ indexmongodb.js
@@ -35,147 +35,147 @@ async function main() {
     };
     //await createListing(client, newListing);
     //@Validator schema
-    db.createCollection( "Patient", {
-      validator: {
-        $jsonSchema: {
-          bsonType: "object",
-          title: "Patient",
-          required: [
-            "pid",
-            "name",
-            "age",
-            "gender",
-            "weight",
-            "riskFactors",
-            "concurrentMedicalConditions",
-          ],
-          properties: {
-            pid: {
-              bsonType: "int",
-              description: "must be an integer and is required",
-            },
-            name: {
-              bsonType: "string",
-              description: "must be a string and is required",
-            },
-            age: {
-              bsonType: "int",
-              description: "must be an integer and is required",
-            },
-            gender: {
-              bsonType: "string",
-              description: "must be a string and is required",
-            },
-            weight: {
-              bsonType: "int",
-              description: "must be an integer and is required",
-            },
-            riskFactors: {
-              bsonType: "object",
-              required: [
-                "isPregnent",
-                "pregnencyCondition",
-                "isSmoking",
-                "isAlcohol",
-                "frequency",
-                "isDrugAllergic",
-                "names",
-              ],
-              properties: {
-                isPregnent: {
-                  bsonType: "boolean",
-                  description: "must be a boolean and is required",
-                },
-                pregnencyCondition: {
-                  bsonType: "string",
-                  description: "must be a string and is required",
-                },
-                isSmoking: {
-                  bsonType: "boolean",
-                  description: "must be a boolean and is required",
-                },
-                isAlcohol: {
-                  bsonType: "boolean",
-                  description: "must be a boolean and is required",
-                },
-                frequency: {
-                  bsonType: "string",
-                  description: "must be a string and is required",
-                },
-                isDrugAllergic: {
-                  bsonType: "boolean",
-                  description: "must be a boolean and is required",
-                },
-                names: {
-                  bsonType: "array",
-                  description: "must be an array and is required",
-                  items: {
-                    bsonType: "string",
-                  },
-                },
-              },
-            },
-            concurrentMedicalConditions: {
-              bsonType: "object",
-              required: ["nameOfMedicalCondition"],
-              properties: {
-                nameOfMedicalCondition: {
-                  bsonType: "array",
-                  description: "must be an array and is required",
-                  items: {
-                    bsonType: "object",
-                    required: ["name", "diagnosisDate", "treatmentMedications"],
-                    properties: {
-                      name: {
-                        bsonType: "string",
-                        description: "must be a string and is required",
-                      },
-                      diagnosisDate: {
-                        bsonType: "string", 
-                        description: "must be a string and is required",
-                      },
-                      treatmentMedications: {
-                        bsonType: "array",
-                        description: "must be an array and is required",
-                        items: {
-                          bsonType: "string",
-                        },
-                      },
-                    },
-                  },
-                },
-              },
-            },
-          },
-        },
-      },
-    });
-    db.collection("info").Patient.insertOne({
-      pid: 1234,
-      name: "vivek",
-      age: 23,
-      gender:'Male',
-      weight: 65,
-      riskFactors: {
-        isPregnent: false,
-        pregnencyCondition: "No",
-        isSmoking: true,
-        isAlcohol: true,
-        frequency: "Daily",
-        isDrugAllergic: true,
-        names: ["Paracetamol", "Aspirin", "Ibuprofen"],
-      },
-      concurrentMedicalConditions: {
-        nameOfMedicalCondition: [
-          {
-            name: "Diabetes",
-            diagnosisDate: "12/12/2012",
-            treatmentMedications: ["Metformin", "Insulin"],
-          },],
-      }
+    // db.createCollection( "Patient", {
+    //   validator: {
+    //     $jsonSchema: {
+    //       bsonType: "object",
+    //       title: "Patient",
+    //       required: [
+    //         "pid",
+    //         "name",
+    //         "age",
+    //         "gender",
+    //         "weight",
+    //         "riskFactors",
+    //         "concurrentMedicalConditions",
+    //       ],
+    //       properties: {
+    //         pid: {
+    //           bsonType: "int",
+    //           description: "must be an integer and is required",
+    //         },
+    //         name: {
+    //           bsonType: "string",
+    //           description: "must be a string and is required",
+    //         },
+    //         age: {
+    //           bsonType: "int",
+    //           description: "must be an integer and is required",
+    //         },
+    //         gender: {
+    //           bsonType: "string",
+    //           description: "must be a string and is required",
+    //         },
+    //         weight: {
+    //           bsonType: "int",
+    //           description: "must be an integer and is required",
+    //         },
+    //         riskFactors: {
+    //           bsonType: "object",
+    //           required: [
+    //             "isPregnent",
+    //             "pregnencyCondition",
+    //             "isSmoking",
+    //             "isAlcohol",
+    //             "frequency",
+    //             "isDrugAllergic",
+    //             "names",
+    //           ],
+    //           properties: {
+    //             isPregnent: {
+    //               bsonType: "boolean",
+    //               description: "must be a boolean and is required",
+    //             },
+    //             pregnencyCondition: {
+    //               bsonType: "string",
+    //               description: "must be a string and is required",
+    //             },
+    //             isSmoking: {
+    //               bsonType: "boolean",
+    //               description: "must be a boolean and is required",
+    //             },
+    //             isAlcohol: {
+    //               bsonType: "boolean",
+    //               description: "must be a boolean and is required",
+    //             },
+    //             frequency: {
+    //               bsonType: "string",
+    //               description: "must be a string and is required",
+    //             },
+    //             isDrugAllergic: {
+    //               bsonType: "boolean",
+    //               description: "must be a boolean and is required",
+    //             },
+    //             names: {
+    //               bsonType: "array",
+    //               description: "must be an array and is required",
+    //               items: {
+    //                 bsonType: "string",
+    //               },
+    //             },
+    //           },
+    //         },
+    //         concurrentMedicalConditions: {
+    //           bsonType: "object",
+    //           required: ["nameOfMedicalCondition"],
+    //           properties: {
+    //             nameOfMedicalCondition: {
+    //               bsonType: "array",
+    //               description: "must be an array and is required",
+    //               items: {
+    //                 bsonType: "object",
+    //                 required: ["name", "diagnosisDate", "treatmentMedications"],
+    //                 properties: {
+    //                   name: {
+    //                     bsonType: "string",
+    //                     description: "must be a string and is required",
+    //                   },
+    //                   diagnosisDate: {
+    //                     bsonType: "string", 
+    //                     description: "must be a string and is required",
+    //                   },
+    //                   treatmentMedications: {
+    //                     bsonType: "array",
+    //                     description: "must be an array and is required",
+    //                     items: {
+    //                       bsonType: "string",
+    //                     },
+    //                   },
+    //                 },
+    //               },
+    //             },
+    //           },
+    //         },
+    //       },
+    //     },
+    //   },
+    // });
+    // db.collection("info").Patient.insertOne({
+    //   pid: 1234,
+    //   name: "vivek",
+    //   age: 23,
+    //   gender:'Male',
+    //   weight: 65,
+    //   riskFactors: {
+    //     isPregnent: false,
+    //     pregnencyCondition: "No",
+    //     isSmoking: true,
+    //     isAlcohol: true,
+    //     frequency: "Daily",
+    //     isDrugAllergic: true,
+    //     names: ["Paracetamol", "Aspirin", "Ibuprofen"],
+    //   },
+    //   concurrentMedicalConditions: {
+    //     nameOfMedicalCondition: [
+    //       {
+    //         name: "Diabetes",
+    //         diagnosisDate: "12/12/2012",
+    //         treatmentMedications: ["Metformin", "Insulin"],
+    //       },],
+    //   }
 
 
-    })
+    // })
     //await findOneListingByName(client, "Raj");
   } catch (e) {
     console.error(e);
